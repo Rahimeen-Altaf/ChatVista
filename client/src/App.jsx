e6693f9ea3c6250386cde13a3990d5f4878fa765
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2024-07-04 01:28:42 +0500

    login/signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import {lazy} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+const Home = lazy(() => import('./pages/Home.jsx'))
+const Login = lazy(() => import('./pages/Login.jsx'))
+const Chat = lazy(() => import('./pages/Chat.jsx'))
+const Groups = lazy(() => import('./pages/Groups.jsx')) 
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatid" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
