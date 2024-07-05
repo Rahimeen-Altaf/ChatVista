ed320303a6262679c1fe7e6ef163913ba4bb8879
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2024-07-05 21:55:07 +0500

    Loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,38 @@
-import {lazy} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ProtectRoute from './components/auth/ProtectRoute.jsx'
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute.jsx";
+import { LayoutLoader } from "./components/layout/Loaders.jsx";
 
-const Home = lazy(() => import('./pages/Home.jsx'))
-const Login = lazy(() => import('./pages/Login.jsx'))
-const Chat = lazy(() => import('./pages/Chat.jsx'))
-const Groups = lazy(() => import('./pages/Groups.jsx')) 
-const NotFound = lazy(() => import('./pages/NotFound.jsx')) 
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const Chat = lazy(() => import("./pages/Chat.jsx"));
+const Groups = lazy(() => import("./pages/Groups.jsx"));
+const NotFound = lazy(() => import("./pages/NotFound.jsx"));
 
-let user = true; 
+let user = true;
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<ProtectRoute user={user}/>}>
-        <Route path="/" element={<Home />}/>
-        <Route path="/chat/:chatid" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
-        </Route>
-
-        <Route path="/login" element={<ProtectRoute user={!user} redirect='/'>
-          <Login />
-        </ProtectRoute>} />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<LayoutLoader/>}>
+        <Routes>
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatid" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-  )
-}
-export default App
+  );
+};
+export default App;
