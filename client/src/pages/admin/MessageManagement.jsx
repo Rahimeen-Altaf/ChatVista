b28c0086717e7714ec26a8e509eb6a85e29e1b8c
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2024-07-19 04:58:30 +0500

    client done

--- client/src/pages/admin/MessageManagement.jsx
+++ client/src/pages/admin/MessageManagement.jsx
@@ -1,11 +1,11 @@
-import { Avatar, Stack, Box } from "@mui/material";
+import { Avatar, Box, Stack } from "@mui/material";
+import moment from "moment";
 import { useEffect, useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import RenderAttachment from "../../components/shared/RenderAttachment";
 import Table from "../../components/shared/Table";
 import { dashboardData } from "../../constants/sampleData";
 import { fileFormat, transformImage } from "../../lib/features";
-import moment from "moment";
-import RenderAttachment from "../../components/shared/RenderAttachment";
 
 const columns = [
   {
@@ -22,26 +22,27 @@ const columns = [
     renderCell: (params) => {
       const { attachments } = params.row;
 
-      return attachments?.length ? attachments.map((i, index) => {
-        const url = i.url;
-        const file = fileFormat(url);
+      return attachments?.length
+        ? attachments.map((i, index) => {
+            const url = i.url;
+            const file = fileFormat(url);
 
-        return (
-          <Box key={index}>
-            <a
-              href={url}
-              download
-              target="_blank"
-              style={{
-                color: "black",
-              }}
-            >
-              {RenderAttachment(file, url)}
-            </a>
-          </Box>
-        );
-      })
-        : "No Attachments"
+            return (
+              <Box key={index}>
+                <a
+                  href={url}
+                  download
+                  target="_blank"
+                  style={{
+                    color: "black",
+                  }}
+                >
+                  {RenderAttachment(file, url)}
+                </a>
+              </Box>
+            );
+          })
+        : "No Attachments";
     },
   },
   {
@@ -57,10 +58,7 @@ const columns = [
     width: 200,
     renderCell: (params) => (
       <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
-        <Avatar
-          src={params.row.sender.avatar}
-          alt={params.row.sender.name}
-        />
+        <Avatar src={params.row.sender.avatar} alt={params.row.sender.name} />
         <span>{params.row.sender.name}</span>
       </Stack>
     ),
@@ -104,7 +102,12 @@ const MessageManagement = () => {
 
   return (
     <AdminLayout>
-      <Table heading={"All Messages"} columns={columns} rows={rows} rowHeight={200} />
+      <Table
+        heading={"All Messages"}
+        columns={columns}
+        rows={rows}
+        rowHeight={200}
+      />
     </AdminLayout>
   );
 };
