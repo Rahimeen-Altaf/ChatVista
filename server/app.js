fded762472e7f46ff8c36c30320656b45cdf3067
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2024-07-23 22:47:14 +0500

    socket.io implement

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/events.js";
+import { getSockets } from "./lib/helper.js";
 import { errorMiddleware } from "./middlewares/error.js";
+import { Message } from "./models/message.js";
 import { connectDb } from "./utils/features.js";
 
+import adminRoute from "./routes/admin.js";
 import chatRoute from "./routes/chat.js";
 import userRoute from "./routes/user.js";
-import adminRoute from "./routes/admin.js";
 
 dotenv.config({
   path: "./.env",
@@ -16,10 +22,13 @@ const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
 const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
 const adminSecretKey = process.env.ADMIN_SECRET_KEY || "Rahimeen Altaf";
+const userSocketIDs = new Map();
 
 connectDb(mongoURI);
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {});
 
 // Using Middleware Here
 app.use(express.json());
@@ -33,10 +42,59 @@ app.get("/", (req, res) => {
   res.send("ChatVista App!");
 });
 
+io.use((socket, next) => {});
+
+io.on("connection", (socket) => {
+  const user = {
+    _id: "123",
+    name: "Rahimeen",
+  };
+  userSocketIDs.set(user._id.toString(), socket.id);
+
+  console.log("a user connected", socket.id);
+
+  socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+    const messageForRealTime = {
+      content: message,
+      _id: uuidv4(),
+      sender: {
+        _id: user._id,
+        name: user.name,
+      },
+      chat: chatId,
+      createdAt: new Date().toISOString(),
+    };
+
+    const messageForDB = {
+      content: message,
+      sender: user._id,
+      chat: chatId,
+    };
+
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(NEW_MESSAGE, {
+      chatId,
+      message: messageForRealTime,
+    });
+    io.to(membersSocket).emit(NEW_MESSAGE_ALERT, { chatId });
+
+    try {
+      await Message.create(messageForDB);
+    } catch (error) {
+      console.log("Error while saving message to DB", error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    userSocketIDs.delete(user._id.toString());
+  });
+});
+
 app.use(errorMiddleware);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port} in ${envMode} mode`);
 });
 
-export { adminSecretKey, envMode };
+export { adminSecretKey, envMode, userSocketIDs };
