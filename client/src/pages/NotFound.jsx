4e2f3fc397006c9f4cb626d797a7c95251efe778
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2024-07-30 07:22:08 +0500

    not found page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,23 @@
+import { Error as ErrorIcon } from "@mui/icons-material";
+import { Container, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>Not Found</div>
-  )
-}
+    <Container maxWidth="lg" sx={{ height: "100vh" }}>
+      <Stack
+        alignItems={"center"}
+        justifyContent={"center"}
+        spacing={"2rem"}
+        height="100%"
+      >
+        <ErrorIcon sx={{ fontSize: "10rem" }} />
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h3">Page Not Found</Typography>
+        <Link to="/">Go back to the home page</Link>
+      </Stack>
+    </Container>
+  );
+};
 
-export default NotFound
+export default NotFound;
