a476d8b77bef2eaed8873fdb332fb801e443db1b
Rahimeen Altaf <abbasirahimeen@gmail.com>
2024-12-19 02:46:57 +0500

    Test

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,9 +1,10 @@
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-  ],
+  // origin: [
+  //   "http://localhost:5173",
+  //   "http://localhost:4173",
+  //   process.env.CLIENT_URL,
+  // ],
+  origin: true, // This will allow all origins
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   credentials: true,
